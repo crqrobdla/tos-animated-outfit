0b9d2f742a89535b5e71135b4b9200fced03dbd9
crqrobdla <s25880260@gmail.com>
2024-09-04 21:07:32 +0800

    test

---
+++ videoData.js
@@ -0,0 +1,28 @@
+// videoData.js
+
+// 定義影片資料的物件
+const videoData = {
+    Vatican: [
+        {
+            title: "Vatican1_idle",
+            url: "https://files.catbox.moe/wlmldl.mp4"
+        },
+        {
+            title: "Vatican1_attack",
+            url: "https://files.catbox.moe/3t6gyy.mp4"
+        }
+    ],
+    lindauyu: [
+        {
+            title: "lindaiyu_idle",
+            url: "https://files.catbox.moe/k6zoxl.mp4"
+        },
+        {
+            title: "lindaiyu_attack",
+            url: "https://files.catbox.moe/ldu5y1.mp4"
+        }
+    ]
+};
+
+// 將影片資料輸出
+export default videoData;
