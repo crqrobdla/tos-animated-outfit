9599814971996efc8488996005b29b0cee10b40c
crqrobdla <s25880260@gmail.com>
2024-09-05 14:03:39 +0800

    test

--- videoData.js
+++ videoData.js
@@ -2,26 +2,90 @@
 
 // 定義影片資料的物件
 const videoData = {
-    Vatican: [
-        {
-            title: "Vatican1_idle",
-            url: "https://files.catbox.moe/wlmldl.mp4"
-        },
-        {
-            title: "Vatican1_attack",
-            url: "https://files.catbox.moe/3t6gyy.mp4"
-        }
-    ],
-    lindauyu: [
-        {
-            title: "lindaiyu_idle",
-            url: "https://files.catbox.moe/k6zoxl.mp4"
-        },
-        {
-            title: "lindaiyu_attack",
-            url: "https://files.catbox.moe/ldu5y1.mp4"
-        }
-    ]
+    character: {
+        Vatican: [
+            {
+                title: "Vatican1_idle",
+                url: "https://files.catbox.moe/wlmldl.mp4"
+            },
+            {
+                title: "Vatican1_attack",
+                url: "https://files.catbox.moe/3t6gyy.mp4"
+            }
+        ],
+        lindauyu: [
+            {
+                title: "lindaiyu_idle",
+                url: "https://files.catbox.moe/k6zoxl.mp4"
+            },
+            {
+                title: "lindaiyu_attack",
+                url: "https://files.catbox.moe/ldu5y1.mp4"
+            }
+        ]
+    },
+    baseroomcharacter: {
+        LIN_edit: [
+            {
+                title: "LIN_edit_Idle1",
+                url: "https://files.catbox.moe/svlxb6.gif"
+            },
+            {
+                title: "LIN_edit_Idle2",
+                url: "https://files.catbox.moe/97u7sw.gif"
+            },
+            {
+                title: "LIN_edit_PickUp",
+                url: "https://files.catbox.moe/u3uz7p.gif"
+            },
+            {
+                title: "LIN_edit_Reaction",
+                url: "https://files.catbox.moe/sdngz7.gif"
+            },
+            {
+                title: "LIN_edit_Touch1",
+                url: "https://files.catbox.moe/1072i1.gif"
+            },
+            {
+                title: "LIN_edit_Touch2",
+                url: "https://files.catbox.moe/dc255m.gif"
+            },
+            {
+                title: "LIN_edit_Walk",
+                url: "https://files.catbox.moe/of4qyf.gif"
+            }
+        ],
+        Nocturne0318B: [
+            {
+                title: "Nocturne0318B_Idle1",
+                url: "https://files.catbox.moe/w39w9d.gif"
+            },
+            {
+                title: "Nocturne0318B_Idle2",
+                url: "https://files.catbox.moe/2e7gsp.gif"
+            },
+            {
+                title: "Nocturne0318B_PickUp",
+                url: "https://files.catbox.moe/7jk110.gif"
+            },
+            {
+                title: "Nocturne0318B_Reaction",
+                url: "https://files.catbox.moe/w4x8cn.gif"
+            },
+            {
+                title: "Nocturne0318B_Touch1",
+                url: "https://files.catbox.moe/rosmre.gif"
+            },
+            {
+                title: "Nocturne0318B_Touch2",
+                url: "https://files.catbox.moe/oyp1mq.gif"
+            },
+            {
+                title: "Nocturne0318B_Walk",
+                url: "https://files.catbox.moe/kq726v.gif"
+            }
+        ]
+    }
 };
 
 // 將影片資料輸出
