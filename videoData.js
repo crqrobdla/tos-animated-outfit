397169dd2b91ea38725e912d97e5e153be2f30b1
crqrobdla <s25880260@gmail.com>
2024-09-12 10:51:03 +0800

    Update videoData.js

--- videoData.js
+++ videoData.js
@@ -21,7 +21,7 @@ const videoData = {
                 url: "https://files.catbox.moe/cj87za.mov"
             }, {
                 title: "nero_attack",
-                url: "https://files.catbox.moe/egzmt2.mov"
+                url: "https://files.catbox.moe/elzff5.mov"
             }
         ]
     },
