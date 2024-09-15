5f39f5f1fb69eb60c975ca1c34730de409b02401
crqrobdla <s25880260@gmail.com>
2024-09-15 10:26:11 +0800

    Update videoData.js

--- videoData.js
+++ videoData.js
@@ -41,6 +41,22 @@ const videoData = {
     },
     baseroomcharacter: {
         categories: [{
+                category: "Shroy_sep",
+                src: "https://files.catbox.moe/p5vuik.png",
+                alt: "Shroy_sep"
+            }, {
+                category: "OureaV2_test",
+                src: "https://files.catbox.moe/cscjna.png",
+                alt: "OureaV2_test"
+            }, {
+                category: "Melody0802merge",
+                src: "https://files.catbox.moe/omv8m5.png",
+                alt: "Melody0802merge"
+            }, {
+                category: "Snow_0727",
+                src: "https://files.catbox.moe/9t0a1j.png",
+                alt: "Snow_0727"
+            }, {
                 category: "BlackGaGa_edit",
                 src: "https://files.catbox.moe/z1wqei.png",
                 alt: "BlackGaGa_edit"
@@ -54,69 +70,161 @@ const videoData = {
                 alt: "LIN_edit"
             }
         ],
+		Shroy_sep: [{
+                title: "Idle1",
+                url: "https://files.catbox.moe/gjxmff.gif"
+            }, {
+                title: "Idle2",
+                url: "https://files.catbox.moe/r10ar4.gif"
+            }, {
+                title: "PickUp",
+                url: "https://files.catbox.moe/t56og3.gif"
+            }, {
+                title: "Reaction",
+                url: "https://files.catbox.moe/0vdokc.gif"
+            }, {
+                title: "Touch1",
+                url: "https://files.catbox.moe/1u3pga.gif"
+            }, {
+                title: "Touch2",
+                url: "https://files.catbox.moe/eoqhh8.gif"
+            }, {
+                title: "Walk",
+                url: "https://files.catbox.moe/001ksb.gif"
+            }
+        ],
+		OureaV2_test: [{
+                title: "Idle1",
+                url: "https://files.catbox.moe/omx0qf.gif"
+            }, {
+                title: "Idle2",
+                url: "https://files.catbox.moe/eni5d4.gif"
+            }, {
+                title: "PickUp",
+                url: "https://files.catbox.moe/4cjvv6.gif"
+            }, {
+                title: "Reaction",
+                url: "https://files.catbox.moe/ghvmft.gif"
+            }, {
+                title: "Touch1",
+                url: "https://files.catbox.moe/u9bg2r.gif"
+            }, {
+                title: "Touch2",
+                url: "https://files.catbox.moe/cys15r.gif"
+            }, {
+                title: "Unuse_Idle1",
+                url: "https://files.catbox.moe/gfwm95.gif"
+            }, {
+                title: "Unuse_Touch1",
+                url: "https://files.catbox.moe/wv85bv.gif"
+            }, {
+                title: "Walk",
+                url: "https://files.catbox.moe/rsz9mt.gif"
+            }
+        ],
+		Melody0802merge: [{
+                title: "Idle1",
+                url: "https://files.catbox.moe/1k5hp9.gif"
+            }, {
+                title: "Idle2",
+                url: "https://files.catbox.moe/naclv3.gif"
+            }, {
+                title: "PickUp",
+                url: "https://files.catbox.moe/iv41ji.gif"
+            }, {
+                title: "Touch1",
+                url: "https://files.catbox.moe/8dr6aj.gif"
+            }, {
+                title: "Touch2",
+                url: "https://files.catbox.moe/og049g.gif"
+            }, {
+                title: "Walk",
+                url: "https://files.catbox.moe/5m0ht9.gif"
+            }
+        ],
+		Snow_0727: [{
+                title: "Idle1",
+                url: "https://files.catbox.moe/mqb6wc.gif"
+            }, {
+                title: "Idle2",
+                url: "https://files.catbox.moe/1ylj4g.gif"
+            }, {
+                title: "PickUp",
+                url: "https://files.catbox.moe/bsyyzc.gif"
+            }, {
+                title: "Touch1",
+                url: "https://files.catbox.moe/putn9q.gif"
+            }, {
+                title: "Touch2",
+                url: "https://files.catbox.moe/xcxva7.gif"
+            }, {
+                title: "Walk",
+                url: "https://files.catbox.moe/2gbdzf.gif"
+            }
+        ],
         LIN_edit: [{
-                title: "LIN_edit_Idle1",
+                title: "Idle1",
                 url: "https://files.catbox.moe/wfidjs.gif"
             }, {
-                title: "LIN_edit_Idle2",
+                title: "Idle2",
                 url: "https://files.catbox.moe/kvuxkc.gif"
             }, {
-                title: "LIN_edit_PickUp",
+                title: "PickUp",
                 url: "https://files.catbox.moe/vmazjc.gif"
             }, {
-                title: "LIN_edit_Reaction",
+                title: "Reaction",
                 url: "https://files.catbox.moe/fbosdg.gif"
             }, {
-                title: "LIN_edit_Touch1",
+                title: "Touch1",
                 url: "https://files.catbox.moe/jb9awe.gif"
             }, {
-                title: "LIN_edit_Touch2",
+                title: "Touch2",
                 url: "https://files.catbox.moe/9kt59o.gif"
             }, {
-                title: "LIN_edit_Walk",
+                title: "Walk",
                 url: "https://files.catbox.moe/v0c6hx.gif"
             }
         ],
         Nocturne0318B: [{
-                title: "Nocturne0318B_Idle1",
+                title: "Idle1",
                 url: "https://files.catbox.moe/nzceiq.gif"
             }, {
-                title: "Nocturne0318B_Idle2",
+                title: "Idle2",
                 url: "https://files.catbox.moe/vz9b5b.gif"
             }, {
-                title: "Nocturne0318B_PickUp",
+                title: "PickUp",
                 url: "https://files.catbox.moe/7e4fis.gif"
             }, {
-                title: "Nocturne0318B_Reaction",
+                title: "Reaction",
                 url: "https://files.catbox.moe/n6e17r.gif"
             }, {
-                title: "Nocturne0318B_Touch1",
+                title: "Touch1",
                 url: "https://files.catbox.moe/ko67xu.gif"
             }, {
-                title: "Nocturne0318B_Touch2",
+                title: "Touch2",
                 url: "https://files.catbox.moe/npxhnp.gif"
             }, {
-                title: "Nocturne0318B_Walk",
+                title: "Walk",
                 url: "https://files.catbox.moe/krnkti.gif"
             }
         ],
         BlackGaGa_edit: [{
-                title: "BlackGaGa_edit_Idle1",
+                title: "Idle1",
                 url: "https://files.catbox.moe/fi5miw.gif"
             }, {
-                title: "BlackGaGa_edit_Idle2",
+                title: "Idle2",
                 url: "https://files.catbox.moe/r8odf5.gif"
             }, {
-                title: "BlackGaGa_edit_PickUp",
+                title: "PickUp",
                 url: "https://files.catbox.moe/kqx6ej.gif"
             }, {
-                title: "BlackGaGa_edit_Touch1",
+                title: "Touch1",
                 url: "https://files.catbox.moe/ls8g8z.gif"
             }, {
-                title: "BlackGaGa_edit_Touch2",
+                title: "Touch2",
                 url: "https://files.catbox.moe/cgcgw6.gif"
             }, {
-                title: "BlackGaGa_edit_Walk",
+                title: "Walk",
                 url: "https://files.catbox.moe/zuj420.gif"
             }
         ]
